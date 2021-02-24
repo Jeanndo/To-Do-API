bddca2e15b0d1a1712486d3cde4868dc68b9c76b
Jeanndo <ukjeando@gmail.com>
2021-02-24 23:54:04 +0200

    Documented using Jsdoc

--- server/utils/appError.js
+++ server/utils/appError.js
@@ -1,12 +1,25 @@
 
 class AppError extends Error{
 
+     /** 
+     * GLOBAL ERROR HANDLER
+     * ___________________________________
+     * @param {Object} message -Global Error Message
+     * @param {Object} statusCode -Status-code comes with error message
+     * 
+     */
     constructor(message,statusCode){
 
         super(message);
-
+       
         this.statusCode = statusCode;
+        /**
+         * @property {string} status -Error message generated according to status code
+         */
         this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+        /**
+         * @property {boolean} isOperational -Indicates Errors happened in production
+         */
         this.isOperational = true;
 
         Error.captureStackTrace(this,this.constructor)
